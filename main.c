34a8d22580ad4ef9098a7b649561b517f4417de3
Tushar <29629980-tusharkoundal91@users.noreply.replit.com>
2024-04-24 12:29:03 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+struct office
+{
+char name [30];
+int id;
+float salary;
+}s[3];
+int main()
+{
+  int i;
+  for (i=0;i<3;i++)
+  {printf("ENTER DETAILS OF EMPLOYEE: ");
+   gets(s[i].name);
+   scanf("%d %f",&s[i].id,&s[i].salary);
+   getchar();
+  }
+  for(i=0;i<3;i++){
+    printf("Details of employee: %s\n %d\n %f\n",s[i].name,s[i].id,s[i].salary);
+  }
+  return 0;
+}
